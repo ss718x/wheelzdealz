dd17814cd1e3e70ed6fc4927a1e63e6ae0fe6c3b
seb lex <ss718@proton.me>
2023-08-22 13:39:13 +0800

    form submit adds new car to database

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -11,5 +11,18 @@ class CarsController < ApplicationController
   end
 
   def create
+    @car = current_user.cars.build(car_params)
+
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_model, :car_info, :car_price)
   end
 end
